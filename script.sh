bc90aa542021e979b8d145f145c0ddedf82dafa0
Khubaib Kasbati <khubaib.kasbati@MacBook-Pro.local>
2023-04-12 15:36:01 +0500

    Added script to start kata

---
+++ script.sh
@@ -0,0 +1,295 @@
+#!/bin/bash
+echo "Country,country-code" >> country-mappings.csv
+git add country-mappings.csv
+git commit -m "Made mapping file"
+echo "Afghanistan,AF" >> country-mappings.csv
+echo "Aland Islands,AX" >> country-mappings.csv
+echo "Albania,AL" >> country-mappings.csv
+echo "Algeria,DZ" >> country-mappings.csv
+echo "American Samoa,AS" >> country-mappings.csv
+echo "Andorra,AD" >> country-mappings.csv
+echo "Angola,AO" >> country-mappings.csv
+echo "Anguilla,AI" >> country-mappings.csv
+echo "Antarctica,AQ" >> country-mappings.csv
+echo "Antigua and Barbuda,AG" >> country-mappings.csv
+echo "Argentina,AR" >> country-mappings.csv
+echo "Armenia,AM" >> country-mappings.csv
+echo "Aruba,AW" >> country-mappings.csv
+echo "Australia,AU" >> country-mappings.csv
+echo "Austria,AT" >> country-mappings.csv
+echo "Azerbaijan,AZ" >> country-mappings.csv
+git add country-mappings.csv
+git commit -m "Add countries starting from A"
+echo "Bahamas,BS" >> country-mappings.csv
+echo "Bahrain,BH" >> country-mappings.csv
+echo "Bangladesh,BD" >> country-mappings.csv
+echo "Barbados,BB" >> country-mappings.csv
+echo "Belarus,BY" >> country-mappings.csv
+echo "Belgium,BE" >> country-mappings.csv
+echo "Belize,BZ" >> country-mappings.csv
+echo "Benin,BJ" >> country-mappings.csv
+echo "Bermuda,BM" >> country-mappings.csv
+echo "Bhutan,BT" >> country-mappings.csv
+echo "Bolivia,BO" >> country-mappings.csv
+echo "Bosnia and Herzegovina,BA" >> country-mappings.csv
+echo "Botswana,BW" >> country-mappings.csv
+echo "Bouvet Island,BV" >> country-mappings.csv
+echo "Brazil,BR" >> country-mappings.csv
+echo "British Virgin Islands,VG" >> country-mappings.csv
+echo "British Indian Ocean Territory,IO" >> country-mappings.csv
+echo "Brunei Darussalam,BN" >> country-mappings.csv
+echo "Bulgaria,BG" >> country-mappings.csv
+echo "Burkina Faso,BF" >> country-mappings.csv
+echo "Burundi,BI" >> country-mappings.csv
+git add country-mappings.csv
+git commit -m "Add countries starting from B"
+echo "Cambodia,KH" >> country-mappings.csv
+echo "Cameroon,CM" >> country-mappings.csv
+echo "Canada,CA" >> country-mappings.csv
+echo "Cape Verde,CV" >> country-mappings.csv
+echo "Cayman Islands,KY" >> country-mappings.csv
+echo "Central African Republic,CF" >> country-mappings.csv
+echo "Chad,TD" >> country-mappings.csv
+echo "Chile,CL" >> country-mappings.csv
+echo "China,CN" >> country-mappings.csv
+echo "Hong Kong,HK" >> country-mappings.csv
+echo "Macao,MO" >> country-mappings.csv
+echo "Christmas Island,CX" >> country-mappings.csv
+echo "Cocos (Keeling) Islands,CC" >> country-mappings.csv
+echo "Colombia,CO" >> country-mappings.csv
+echo "Comoros,KM" >> country-mappings.csv
+echo "Congo (Brazzaville),CG" >> country-mappings.csv
+echo "Congo (Kinshasa),CD" >> country-mappings.csv
+echo "Cook Islands,CK" >> country-mappings.csv
+echo "Costa Rica,CR" >> country-mappings.csv
+echo "Côte d'Ivoire,CI" >> country-mappings.csv
+echo "Croatia,HR" >> country-mappings.csv
+echo "Cuba,CU" >> country-mappings.csv
+echo "Cyprus,CY" >> country-mappings.csv
+echo "Czech Republic,CZ" >> country-mappings.csv
+git add country-mappings.csv
+git commit -m "Add countries starting from C"
+echo "Denmark,DK" >> country-mappings.csv
+echo "Djibouti,DJ" >> country-mappings.csv
+echo "Dominica,DM" >> country-mappings.csv
+echo "Dominican Republic,DO" >> country-mappings.csv
+echo "Ecuador,EC" >> country-mappings.csv
+echo "Egypt,EG" >> country-mappings.csv
+echo "El Salvador,SV" >> country-mappings.csv
+echo "Equatorial Guinea,GQ" >> country-mappings.csv
+echo "Eritrea,ER" >> country-mappings.csv
+echo "Estonia,EE" >> country-mappings.csv
+echo "Ethiopia,ET" >> country-mappings.csv
+echo "Falkland Islands (Malvinas),FK" >> country-mappings.csv
+echo "Faroe Islands,FO" >> country-mappings.csv
+echo "Fiji,FJ" >> country-mappings.csv
+echo "Finland,FI" >> country-mappings.csv
+echo "France,FR" >> country-mappings.csv
+echo "French Guiana,GF" >> country-mappings.csv
+echo "French Polynesia,PF" >> country-mappings.csv
+echo "French Southern Territories,TF" >> country-mappings.csv
+git add country-mappings.csv
+git commit -m "Add countries starting from D-F"
+echo "Gabon,GA" >> country-mappings.csv
+echo "Gambia,GM" >> country-mappings.csv
+echo "Georgia,GE" >> country-mappings.csv
+echo "Germany,DE" >> country-mappings.csv
+echo "Ghana,GH" >> country-mappings.csv
+echo "Gibraltar,GI" >> country-mappings.csv
+echo "Greece,GR" >> country-mappings.csv
+echo "Greenland,GL" >> country-mappings.csv
+echo "Grenada,GD" >> country-mappings.csv
+echo "Guadeloupe,GP" >> country-mappings.csv
+echo "Guam,GU" >> country-mappings.csv
+echo "Guatemala,GT" >> country-mappings.csv
+echo "Guernsey,GG" >> country-mappings.csv
+echo "Guinea,GN" >> country-mappings.csv
+echo "Guinea-Bissau,GW" >> country-mappings.csv
+echo "Guyana,GY" >> country-mappings.csv
+echo "Haiti,HT" >> country-mappings.csv
+echo "Heard and Mcdonald Islands,HM" >> country-mappings.csv
+echo "Holy See (Vatican City State),VA" >> country-mappings.csv
+echo "Honduras,HN" >> country-mappings.csv
+echo "Hungary,HU" >> country-mappings.csv
+echo "Iceland,IS" >> country-mappings.csv
+echo "India,IN" >> country-mappings.csv
+echo "Indonesia,ID" >> country-mappings.csv
+echo "Islamic Republic of Iran,IR" >> country-mappings.csv
+echo "Iraq,IQ" >> country-mappings.csv
+echo "Ireland,IE" >> country-mappings.csv
+echo "Isle of Man,IM" >> country-mappings.csv
+echo "Israel,IL" >> country-mappings.csv
+echo "Italy,IT" >> country-mappings.csv
+git add country-mappings.csv
+git commit -m "Add countries starting from G-I"
+echo "Jamaica,JM" >> country-mappings.csv
+echo "Japan,JP" >> country-mappings.csv
+echo "Jersey,JE" >> country-mappings.csv
+echo "Jordan,JO" >> country-mappings.csv
+echo "Kazakhstan,KZ" >> country-mappings.csv
+echo "Kenya,KE" >> country-mappings.csv
+echo "Kiribati,KI" >> country-mappings.csv
+echo "North Korea North,KP" >> country-mappings.csv
+echo "South Korea,KR" >> country-mappings.csv
+echo "Kuwait,KW" >> country-mappings.csv
+echo "Kyrgyzstan,KG" >> country-mappings.csv
+echo "Lao PDR,LA" >> country-mappings.csv
+echo "Latvia,LV" >> country-mappings.csv
+echo "Lebanon,LB" >> country-mappings.csv
+echo "Lesotho,LS" >> country-mappings.csv
+echo "Liberia,LR" >> country-mappings.csv
+echo "Libya,LY" >> country-mappings.csv
+echo "Liechtenstein,LI" >> country-mappings.csv
+echo "Lithuania,LT" >> country-mappings.csv
+echo "Luxembourg,LU" >> country-mappings.csv
+echo "Republic of Macedonia,MK" >> country-mappings.csv
+echo "Madagascar,MG" >> country-mappings.csv
+echo "Malawi,MW" >> country-mappings.csv
+echo "Malaysia,MY" >> country-mappings.csv
+echo "Maldives,MV" >> country-mappings.csv
+echo "Mali,ML" >> country-mappings.csv
+echo "Malta,MT" >> country-mappings.csv
+echo "Marshall Islands,MH" >> country-mappings.csv
+echo "Martinique,MQ" >> country-mappings.csv
+echo "Mauritania,MR" >> country-mappings.csv
+echo "Mauritius,MU" >> country-mappings.csv
+echo "Mayotte,YT" >> country-mappings.csv
+echo "Mexico,MX" >> country-mappings.csv
+echo "Federated States of Micronesia,FM" >> country-mappings.csv
+echo "Moldova,MD" >> country-mappings.csv
+echo "Monaco,MC" >> country-mappings.csv
+echo "Mongolia,MN" >> country-mappings.csv
+echo "Montenegro,ME" >> country-mappings.csv
+echo "Montserrat,MS" >> country-mappings.csv
+echo "Morocco,MA" >> country-mappings.csv
+echo "Mozambique,MZ" >> country-mappings.csv
+echo "Myanmar,MM" >> country-mappings.csv
+git add country-mappings.csv
+git commit -m "Add countries starting from J-M"
+echo "Namibia,NA" >> country-mappings.csv
+echo "Nauru,NR" >> country-mappings.csv
+echo "Nepal,NP" >> country-mappings.csv
+echo "Netherlands,NL" >> country-mappings.csv
+echo "Netherlands Antilles,AN" >> country-mappings.csv
+echo "New Caledonia,NC" >> country-mappings.csv
+echo "New Zealand,NZ" >> country-mappings.csv
+echo "Nicaragua,NI" >> country-mappings.csv
+echo "Niger,NE" >> country-mappings.csv
+echo "Nigeria,NG" >> country-mappings.csv
+echo "Niue,NU" >> country-mappings.csv
+echo "Norfolk Island,NF" >> country-mappings.csv
+echo "Northern Mariana Islands,MP" >> country-mappings.csv
+echo "Norway,NO" >> country-mappings.csv
+echo "Oman,OM" >> country-mappings.csv
+echo "Pakistan,PK" >> country-mappings.csv
+echo "Palau,PW" >> country-mappings.csv
+echo "Palestinian Territory,PS" >> country-mappings.csv
+echo "Panama,PA" >> country-mappings.csv
+echo "Papua New Guinea,PG" >> country-mappings.csv
+echo "Paraguay,PY" >> country-mappings.csv
+echo "Peru,PE" >> country-mappings.csv
+echo "Philippines,PH" >> country-mappings.csv
+echo "Pitcairn,PN" >> country-mappings.csv
+echo "Poland,PL" >> country-mappings.csv
+echo "Portugal,PT" >> country-mappings.csv
+echo "Puerto Rico,PR" >> country-mappings.csv
+echo "Qatar,QA" >> country-mappings.csv
+git add country-mappings.csv
+git commit -m "Add countries starting from N-Q"
+echo "Réunion,RE" >> country-mappings.csv
+echo "Romania,RO" >> country-mappings.csv
+echo "Russian Federation,RU" >> country-mappings.csv
+echo "Rwanda,RW" >> country-mappings.csv
+echo "Saint-Barthélemy,BL" >> country-mappings.csv
+echo "Saint Helena,SH" >> country-mappings.csv
+echo "Saint Kitts and Nevis,KN" >> country-mappings.csv
+echo "Saint Lucia,LC" >> country-mappings.csv
+echo "Saint-Martin,MF" >> country-mappings.csv
+echo "Saint Pierre and Miquelon,PM" >> country-mappings.csv
+echo "Saint Vincent and Grenadines,VC" >> country-mappings.csv
+echo "Samoa,WS" >> country-mappings.csv
+echo "San Marino,SM" >> country-mappings.csv
+echo "Sao Tome and Principe,ST" >> country-mappings.csv
+echo "Saudi Arabia,SA" >> country-mappings.csv
+echo "Senegal,SN" >> country-mappings.csv
+echo "Serbia,RS" >> country-mappings.csv
+echo "Seychelles,SC" >> country-mappings.csv
+echo "Sierra Leone,SL" >> country-mappings.csv
+echo "Singapore,SG" >> country-mappings.csv
+echo "Slovakia,SK" >> country-mappings.csv
+echo "Slovenia,SI" >> country-mappings.csv
+echo "Solomon Islands,SB" >> country-mappings.csv
+echo "Somalia,SO" >> country-mappings.csv
+echo "South Africa,ZA" >> country-mappings.csv
+echo "South Georgia and the South Sandwich Islands,GS" >> country-mappings.csv
+echo "South Sudan,SS" >> country-mappings.csv
+echo "Spain,ES" >> country-mappings.csv
+echo "Sri Lanka,LK" >> country-mappings.csv
+echo "Sudan,SD" >> country-mappings.csv
+echo "Suriname,SR" >> country-mappings.csv
+echo "Svalbard and Jan Mayen Islands,SJ" >> country-mappings.csv
+echo "Swaziland,SZ" >> country-mappings.csv
+echo "Sweden,SE" >> country-mappings.csv
+echo "Switzerland,CH" >> country-mappings.csv
+echo "Syria,SY" >> country-mappings.csv
+echo "Taiwan,TW" >> country-mappings.csv
+echo "Tajikistan,TJ" >> country-mappings.csv
+echo "United Republic of Tanzania,TZ" >> country-mappings.csv
+echo "Thailand,TH" >> country-mappings.csv
+echo "Timor-Leste,TL" >> country-mappings.csv
+echo "Togo,TG" >> country-mappings.csv
+echo "Tokelau,TK" >> country-mappings.csv
+echo "Tonga,TO" >> country-mappings.csv
+echo "Trinidad and Tobago,TT" >> country-mappings.csv
+echo "Tunisia,TN" >> country-mappings.csv
+echo "Turkey,TR" >> country-mappings.csv
+echo "Turkmenistan,TM" >> country-mappings.csv
+echo "Turks and Caicos Islands,TC" >> country-mappings.csv
+echo "Tuvalu,TV" >> country-mappings.csv
+git add country-mappings.csv
+git commit -m "Add countries starting from R-T"
+echo "Uganda,UG" >> country-mappings.csv
+echo "Ukraine,UA" >> country-mappings.csv
+echo "United Arab Emirates,AE" >> country-mappings.csv
+echo "United Kingdom,GB" >> country-mappings.csv
+echo "United States of America,US" >> country-mappings.csv
+echo "US Minor Outlying Islands,UM" >> country-mappings.csv
+echo "Uruguay,UY" >> country-mappings.csv
+echo "Uzbekistan,UZ" >> country-mappings.csv
+echo "Vanuatu,VU" >> country-mappings.csv
+echo "Venezuela,VE" >> country-mappings.csv
+echo "Vietnam,VN" >> country-mappings.csv
+echo "Virgin Islands, US,VI" >> country-mappings.csv
+echo "Wallis and Futuna Islands,WF" >> country-mappings.csv
+echo "Western Sahara,EH" >> country-mappings.csv
+echo "Yemen,YE" >> country-mappings.csv
+echo "Zambia,ZM" >> country-mappings.csv
+echo "Zimbabwe,ZW" >> country-mappings.csv
+git add country-mappings.csv
+git commit -m "Add countries starting from U-Z"
+rm country-mappings.csv
+rm main.js
+echo "const express = require('express');" >> main.js
+echo "const app = express();  " >> main.js
+echo "const axios = require('axios');" >> main.js
+echo "" >> main.js
+echo "const API_KEY = '49aa5fba3bea97942b90799f43efa429';" >> main.js
+echo "" >> main.js
+echo "const PORT = process.env.PORT || 3000;" >> main.js
+echo "" >> main.js
+echo "app.listen(PORT, () => {" >> main.js
+echo "  console.log(`Server listening on port ${PORT}`);" >> main.js
+echo "});" >> main.js
+echo "" >> main.js
+echo "app.get('/weather/:city', (req, res) => {" >> main.js
+echo "    const city = req.params.city;" >> main.js
+echo '    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`;' >> main.js
+echo "    axios.get(apiUrl)" >> main.js
+echo "      .then(response => {" >> main.js
+echo "        res.send(response.data);" >> main.js
+echo "      })" >> main.js
+echo "      .catch(error => {" >> main.js
+echo "        console.log(error);" >> main.js
+echo "        res.status(500).send('Error fetching weather data');" >> main.js
+echo "      });" >> main.js
+echo "  });" >> main.js
